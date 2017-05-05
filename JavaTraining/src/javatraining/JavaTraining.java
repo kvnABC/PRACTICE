44efeb1f2b81e4b61292ff4a694aa8704e47d032
Kevin <kvn2794@gmail.com>
2017-05-04 20:16:23 -0600

    Database Connection to APP

--- JavaTraining/src/javatraining/JavaTraining.java
+++ JavaTraining/src/javatraining/JavaTraining.java
@@ -5,6 +5,11 @@
  */
 package javatraining;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  *
  * @author kennethbolivar
@@ -16,6 +21,36 @@ public class JavaTraining {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        Connection conn = null;
+        Properties connectProp = new Properties();
+        connectProp.put("user", "root");
+        connectProp.put("password", "root");
+
+        try{
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:8889/", connectProp);
+         System.out.println(conn.getMetaData().toString());
+         
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        /*try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }*/
+
+       
+
     }
-    
+
 }
